49ee043b0f447589907b96fcb77928c81d56ae45
Jay Williams <jay@myd3.com>
2020-11-08 21:55:14 -0600

    Get example working with an RGBW par light

--- ft232/example/main.go
+++ ft232/example/main.go
@@ -14,12 +14,13 @@ func main() {
 	// as of the next release
 	vid := uint16(0x0403)
 	pid := uint16(0x6001)
-	outputInterfaceID := flag.Int("output-id", 0, "Output interface ID for device")
+	outputInterfaceID := flag.Int("output-id", 2, "Output interface ID for device")
+	inputInterfaceID := flag.Int("input-id", 1, "Input interface ID for device")
 	debugLevel := flag.Int("debug", 0, "Debug level for USB context")
 	flag.Parse()
 
 	// Create a configuration from our flags
-	config := usbdmx.NewConfig(vid, pid, *outputInterfaceID, *debugLevel)
+	config := usbdmx.NewConfig(vid, pid, *outputInterfaceID, *inputInterfaceID, *debugLevel)
 
 	// Get a usb context for our configuration
 	config.GetUSBContext()
@@ -34,18 +35,20 @@ func main() {
 	// fixture, like a par can, will not be showing any light. We're ignoring
 	// errors but the SetChannel function will return an error if it fails to
 	// write to the array
-	controller.SetChannel(1, 0)
-	controller.SetChannel(2, 0)
-	controller.SetChannel(3, 0)
+	controller.SetChannel(1, 0) // Total Dimming
+	controller.SetChannel(2, 0) // Red
+	controller.SetChannel(3, 0) // Green
+	controller.SetChannel(4, 0) // Blue
+	controller.SetChannel(5, 0) // White
+	// controller.SetChannel(6, 0) // Strobe (0-9 Off, 10-255 Strobe Rate)
+	// controller.SetChannel(7, 0) // Mode
 
 	// Create an array of colours for our fixture to switch between
 	colours := [][]byte{
-		[]byte{255, 0, 0},
-		[]byte{255, 255, 0},
-		[]byte{0, 255, 0},
-		[]byte{0, 255, 255},
-		[]byte{0, 0, 255},
-		[]byte{255, 0, 255},
+		[]byte{255, 255, 0, 0, 0},
+		[]byte{255, 0, 255, 0, 0},
+		[]byte{255, 0, 0, 255, 0},
+		[]byte{255, 0, 0, 0, 255},
 	}
 
 	// Create a go routine that will ensure our controller keeps sending data
@@ -69,12 +72,18 @@ func main() {
 		controller.SetChannel(1, colour[0])
 		controller.SetChannel(2, colour[1])
 		controller.SetChannel(3, colour[2])
+		controller.SetChannel(4, colour[3])
+		controller.SetChannel(5, colour[4])
 
-		r, _ := controller.GetChannel(1)
-		g, _ := controller.GetChannel(2)
-		b, _ := controller.GetChannel(3)
+		t, _ := controller.GetChannel(1)
+		r, _ := controller.GetChannel(2)
+		g, _ := controller.GetChannel(3)
+		b, _ := controller.GetChannel(4)
+		w, _ := controller.GetChannel(5)
 
-		log.Printf("Ch1: %d \t Ch2: %d \t Ch3: %d", r, g, b)
-		time.Sleep(time.Second * 2)
+		log.Printf("Ch1: %d \t Ch2: %d \t Ch3: %d \t Ch4: %d \t Ch5: %d", t, r, g, b, w)
+		// time.Sleep(time.Second * 2)
+		//time.Sleep(time.Millisecond * 250)
+		time.Sleep(time.Millisecond * 10)
 	}
 }
